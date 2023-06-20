af8d32fb4f948e59d8b5269534e948c06c23a033
MaragkakisMike <maragkakismike@gmail.com>
2023-06-20 19:34:08 +0300

    FilesManagement and some styling details

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import MoviesPage from "./MoviesPage";
-import SeriesPage from "./SeriesPage";
+import MoviesPage from "./pages/MoviesPage";
+import SeriesPage from "./pages/SeriesPage";
 import {BrowserRouter as Router,Routes, Route, Link} from "react-router-dom";
+import "./App.css";
 
 function App(){
   return  <Router>
     <div className="navBar" id="navBar">
-      <input type="text" id="search" name="search" placeholder="Search a movie/serie.." />
+      <input type="text" id="search" name="search" placeholder="Search movies/series.." />
       <div className="d-grid gap-2">
         <Link to="/"><button className="btn" type="button" id="moviesbtn" >MOVIES</button></Link>
         <Link to="/series"><button className="btn" type="button" id="seriesbtn" >SERIES</button></Link>
